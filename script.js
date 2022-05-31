a7ce28e8368232d05de72049162146062cd29b57
SAVIO PRINCE <69968870+OIVAS7572@users.noreply.github.com>
2022-05-31 18:24:03 +0530

    Dev (#1)
    * Backup
    
    * updated
    
    * Backup
    
    * Optimized & Added Effects
    
    * Update CNAME
    
    * made responsive for small screen
    
    * Cleaned up and Updated
    
    * README.md
    
    Co-authored-by: OIVAS7572 <69968870OIVAS7572@users.noreply.github.com>

---
+++ script.js
@@ -0,0 +1,18 @@
+window.addEventListener('load', function () {
+    var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+    var source = audioCtx.createBufferSource();
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'Creepy');
+    xhr.responseType = 'arraybuffer';
+    xhr.addEventListener('load', function (r) {
+        audioCtx.decodeAudioData(
+            xhr.response,
+            function (buffer) {
+                source.buffer = buffer;
+                source.connect(audioCtx.destination);
+                source.loop = true;
+            });
+        source.start(0);
+    });
+    xhr.send();
+});
